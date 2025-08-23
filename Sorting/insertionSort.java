a248fe4bdfe5882e882cde68564888e0fbd898f6
yadavaman13 <yadavaman8511005211@gmail.com>
2025-08-23 12:33:06 +0530

    insertion sort

---
+++ Sorting/insertionSort.java
@@ -0,0 +1,27 @@
+public class insertionSort {
+    
+    //insertion sort function
+    public static void iSort(int[] arr){
+        int n = arr.length;
+
+        for(int j=1; j<n; j++){
+            int key = arr[j];
+
+            int i = j-1;
+
+            while(i>=0 && arr[i]>key){
+                arr[i+1] = arr[i];
+                i--;
+            }
+            arr[i+1] = key;
+        }
+
+        for(int k=0; k<n; k++){
+            System.out.print(arr[k]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,9,1,2,3,4};
+        iSort(arr);
+    }
+}
