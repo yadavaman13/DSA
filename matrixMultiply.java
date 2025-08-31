f9bf13d3d1d2a68e676682a04eba10b8cbe9ccd0
yadavaman13 <yadavaman8511005211@gmail.com>
2025-08-31 13:47:55 +0530

    fixes

--- matrixMultiply.java
+++ matrixMultiply.java
@@ -6,9 +6,17 @@ public class matrixMultiply{
 
         for(int i=0; i<a.length; i++){
             for(int j=0; j<b.length; j++){
-                c[i][j]=a[i][j]*b[j][i];
+                c[i][j] = 0;
+                for(int k=0; k<c.length; k++){
+                    c[i][j] += a[i][k]*b[k][j];
+                }
             }
         }
 
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<b.length; j++){
+                System.out.print(c[i][j] +" ");
+            }
+        }
     }
 }
